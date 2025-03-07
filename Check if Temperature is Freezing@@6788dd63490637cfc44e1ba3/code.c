7c8b34740ae32d02bab3bf99d0c095cb9377b1d6
12-Harleen <harleen1311.becse24@chitkara.edu.in>
2025-03-07 17:40:28 +0530

    Solution to Check if Temperature is Freezing

--- Check if Temperature is Freezing@@6788dd63490637cfc44e1ba3/code.c
+++ Check if Temperature is Freezing@@6788dd63490637cfc44e1ba3/code.c
@@ -2,36 +2,10 @@
 int main(){
     int x;
     scanf("%d",&x);
-    if(x==1){
-        printf("Monday");
-    }
-    else if(x==2){
-        printf("Tuesday");
-    }
-    else if(x==3){
-        printf("Wednesday");
-    }
-    else if(x==4){
-        printf("Thursday");
-    }
-    else if(x==5){
-        printf("Friday");
-    }
-    else if(x==6){
-        printf("Saturday");
-    }
-    else if(x==7){
-        printf("Sunday");
+    if(x<=0){
+        printf("Freezing");
     }
     else{
-        printf("Invalid");
+        printf("Above Freezing");
     }
-    
-    
-    
-    
-    
-
-    
-    
 }
